7e6f402e65a7e4021d2cd40c3cb6940f7e3a784d
Christian Ambach <christian.ambach@codecentric.de>
2024-08-08 18:15:36 +0200

    initial commit

---
+++ src/main/kotlin/de/bashburg/springbook/catalogservice/HomeController.kt
@@ -0,0 +1,10 @@
+package de.bashburg.springbook.catalogservice
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+    @GetMapping("/")
+    fun getGreeting() = "Welcome to the Book Catalog!"
+}
