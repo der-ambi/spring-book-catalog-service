395d48056639f2a7bae7c8d7c3e5a56d7f9a2e4a
Christian Ambach <christian.ambach@codecentric.de>
2024-08-12 15:57:28 +0200

    chapter 3.4.2 extend SpringBootTest

--- src/test/kotlin/de/bashburg/springbook/catalogservice/CatalogServiceApplicationTests.kt
+++ src/test/kotlin/de/bashburg/springbook/catalogservice/CatalogServiceApplicationTests.kt
@@ -1,13 +1,35 @@
 package de.bashburg.springbook.catalogservice
 
+import de.bashburg.springbook.catalogservice.domain.Book
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CatalogServiceApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
 
+    @Test
+    fun `when post request then book created`() {
+        val expectedBook = Book("1231231231231", "Title", "Author", 9.90)
+
+        webTestClient
+            .post()
+            .uri("/books")
+            .bodyValue(expectedBook)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody<Book>()
+            .consumeWith {
+                val responseBody = it.responseBody
+                assertNotNull(responseBody)
+                assertEquals(expectedBook.isbn, responseBody.isbn)
+            }
+    }
 }
