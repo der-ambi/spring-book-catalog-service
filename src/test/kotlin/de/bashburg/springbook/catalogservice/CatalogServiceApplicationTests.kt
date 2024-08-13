3b381cd4617dd91a673c945c0cd0033d479f0bf5
Christian Ambach <christian.ambach@codecentric.de>
2024-08-13 17:20:51 +0200

    switch existing test to testcontainers

--- src/test/kotlin/de/bashburg/springbook/catalogservice/CatalogServiceApplicationTests.kt
+++ src/test/kotlin/de/bashburg/springbook/catalogservice/CatalogServiceApplicationTests.kt
@@ -1,16 +1,30 @@
 package de.bashburg.springbook.catalogservice
 
+import de.bashburg.springbook.catalogservice.config.DataConfig
 import de.bashburg.springbook.catalogservice.domain.Book
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.annotation.Import
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
+@Testcontainers
+@Import(DataConfig::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CatalogServiceApplicationTests {
+
+    companion object {
+        @Container
+        @ServiceConnection
+        val pg = PostgreSQLContainer("postgres:16")
+    }
 
     @Autowired
     private lateinit var webTestClient: WebTestClient
