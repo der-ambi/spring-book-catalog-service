e4ae10495fa42f1841958d3c44010a84464412de
Christian Ambach <christian.ambach@codecentric.de>
2024-08-14 10:17:42 +0200

    add missing Transactional annotation
    align to the other methods changing data in CrudRepository

--- src/main/kotlin/de/bashburg/springbook/catalogservice/domain/BookRepository.kt
+++ src/main/kotlin/de/bashburg/springbook/catalogservice/domain/BookRepository.kt
@@ -3,12 +3,14 @@ package de.bashburg.springbook.catalogservice.domain
 import org.springframework.data.jdbc.repository.query.Modifying
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.transaction.annotation.Transactional
 
 interface BookRepository : CrudRepository<Book, Long> {
     fun findByIsbn(isbn: String): Book?
     fun existsByIsbn(isbn: String): Boolean
 
     @Modifying
+    @Transactional
     @Query("delete from book where isbn = :isbn")
     fun deleteByIsbn(isbn: String)
 }
