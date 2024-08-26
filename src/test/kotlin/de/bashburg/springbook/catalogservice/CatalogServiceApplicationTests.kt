9083bb3a2dfbb874557d49676c9d925a2ff50112
Christian Ambach <christian.ambach@codecentric.de>
2024-08-26 15:19:16 +0200

    12.2.3 oauth tests

--- src/test/kotlin/de/bashburg/springbook/catalogservice/CatalogServiceApplicationTests.kt
+++ src/test/kotlin/de/bashburg/springbook/catalogservice/CatalogServiceApplicationTests.kt
@@ -1,16 +1,23 @@
 package de.bashburg.springbook.catalogservice
 
-import de.bashburg.springbook.catalogservice.config.DataConfig
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+import dasniko.testcontainers.keycloak.KeycloakContainer
 import de.bashburg.springbook.catalogservice.domain.Book
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection
-import org.springframework.context.annotation.Import
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
-import org.testcontainers.containers.PostgreSQLContainer
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import kotlin.test.assertEquals
@@ -18,18 +25,20 @@ import kotlin.test.assertNotNull
 
 @ActiveProfiles("integration")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class CatalogServiceApplicationTests {
 
     @Autowired
     private lateinit var webTestClient: WebTestClient
 
     @Test
-    fun `when post request then book created`() {
+    fun `when post request as authenticated user with role employee then book created`() {
         val expectedBook = Book.of("1231231231231", "Title", "Author", "Polarsophia", 9.90)
 
         webTestClient
             .post()
             .uri("/books")
+            .headers { it.setBearerAuth(isabelleToken.accessToken) }
             .bodyValue(expectedBook)
             .exchange()
             .expectStatus().isCreated
@@ -39,5 +48,74 @@ class CatalogServiceApplicationTests {
                 assertNotNull(responseBody)
                 assertEquals(expectedBook.isbn, responseBody.isbn)
             }
+    }
+
+    @Test
+    fun `when post request as authenticated user with role customer then 401`() {
+        val expectedBook = Book.of("1231231231231", "Title", "Author", "Polarsophia", 9.90)
+
+        webTestClient
+            .post()
+            .uri("/books")
+            .headers { it.setBearerAuth(bjornToken.accessToken) }
+            .bodyValue(expectedBook)
+            .exchange()
+            .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `when post request is unauthorized then 403`() {
+        val expectedBook = Book.of("1231231231231", "Title", "Author", "Polarsophia", 9.90)
+
+        webTestClient
+            .post()
+            .uri("/books")
+            .bodyValue(expectedBook)
+            .exchange()
+            .expectStatus().isUnauthorized
+    }
+
+    companion object {
+
+        class KeycloakToken @JsonCreator constructor(@JsonProperty("access_token") val accessToken: String)
+
+        private lateinit var bjornToken: KeycloakToken
+        private lateinit var isabelleToken: KeycloakToken
+
+        @Container
+        private val keycloakContainer =
+            KeycloakContainer("quay.io/keycloak/keycloak:25.0").withRealmImportFile("test-realm-config.json")
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun staticProperties(dynamicPropertyRegistry: DynamicPropertyRegistry) =
+            dynamicPropertyRegistry.add(
+                "spring.security.oauth2.resourceserver.jwt.issuer-uri",
+                { "${keycloakContainer.authServerUrl}/realms/PolarBookshop" })
+
+        @JvmStatic
+        @BeforeAll
+        fun generateAccessTokens(): Unit {
+            val webClient = WebClient.builder()
+                .baseUrl("${keycloakContainer.authServerUrl}/realms/PolarBookshop/protocol/openid-connect/token")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .build()
+
+            this.isabelleToken = authenticateWith("isabelle", "password", webClient)
+            this.bjornToken = authenticateWith("bjorn", "password", webClient)
+        }
+
+        fun authenticateWith(userName: String, password: String, webClient: WebClient): KeycloakToken =
+            webClient
+                .post()
+                .body(
+                    BodyInserters
+                        .fromFormData("grant_type", "password")
+                        .with("client_id", "polar-test")
+                        .with("username", userName)
+                        .with("password", password)
+                ).retrieve()
+                .bodyToMono<KeycloakToken>()
+                .block()!!
     }
 }
--- src/test/kotlin/de/bashburg/springbook/catalogservice/web/BookControllerMvcTests.kt
+++ src/test/kotlin/de/bashburg/springbook/catalogservice/web/BookControllerMvcTests.kt
@@ -1,5 +1,6 @@
 package de.bashburg.springbook.catalogservice.web
 
+import de.bashburg.springbook.catalogservice.config.SecurityConfig
 import de.bashburg.springbook.catalogservice.domain.BookNotFoundException
 import de.bashburg.springbook.catalogservice.domain.BookService
 import org.junit.jupiter.api.Test
@@ -7,15 +8,24 @@ import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(BookController::class)
-class BookControllerMvcTests() {
+@Import(SecurityConfig::class)
+class BookControllerMvcTests {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private lateinit var jwtDecoder: JwtDecoder
 
     @MockBean
     private lateinit var bookService: BookService
@@ -28,5 +38,31 @@ class BookControllerMvcTests() {
         mockMvc
             .perform(get("/books/$isbn"))
             .andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun `when delete book with employee role then should return 204`() {
+        val isbn = "7373737313940"
+        mockMvc.perform(
+            delete("/books/$isbn", isbn)
+                .with(jwt().authorities(SimpleGrantedAuthority("ROLE_employee")))
+        ).andExpect(status().isNoContent)
+    }
+
+    @Test
+    fun `when delete book with customer role then should return 403`() {
+        val isbn = "7373737313940"
+        mockMvc.perform(
+            delete("/books/$isbn", isbn)
+                .with(jwt().authorities(SimpleGrantedAuthority("ROLE_customer")))
+        ).andExpect(status().isForbidden)
+    }
+
+    @Test
+    fun `when delete book not authenticated then should return 401`() {
+        val isbn = "7373737313940"
+        mockMvc.perform(
+            delete("/books/$isbn", isbn)
+        ).andExpect(status().isUnauthorized)
     }
 }
