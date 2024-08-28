55cfec85434ad1c76d280702568199d3cf3cffaf
Christian Ambach <christian.ambach@codecentric.de>
2024-08-28 12:30:08 +0200

    chapter 13.1 add some more logs

--- src/main/kotlin/de/bashburg/springbook/catalogservice/domain/BookService.kt
+++ src/main/kotlin/de/bashburg/springbook/catalogservice/domain/BookService.kt
@@ -1,22 +1,33 @@
 package de.bashburg.springbook.catalogservice.domain
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Service
+
+private val logger = KotlinLogging.logger {}
 
 @Service
 class BookService(val bookRepository: BookRepository) {
-    fun viewBookList(): Iterable<Book> = bookRepository.findAll()
+    fun viewBookList(): Iterable<Book> {
+        logger.info { "Fetching the list of books in the catalog" }
+        return bookRepository.findAll()
+    }
 
     fun viewBookDetails(isbn: String): Book =
         bookRepository.findByIsbn(isbn) ?: throw BookNotFoundException(isbn)
 
     fun addBookToCatalog(book: Book): Book {
         if (bookRepository.existsByIsbn(book.isbn)) {
+            logger.warn { "Book with ISBN ${book.isbn} could not be added as it already exists" }
             throw BookAlreadyExistsException(book.isbn)
         }
+        logger.info { "Added book $book to catalog" }
         return bookRepository.save(book)
     }
 
-    fun removeBookFromCatalog(isbn: String) = bookRepository.deleteByIsbn(isbn)
+    fun removeBookFromCatalog(isbn: String) {
+        bookRepository.deleteByIsbn(isbn)
+        logger.info { "Book with ISBN $isbn was deleted from the catalog" }
+    }
 
     fun editBookDetails(isbn: String, book: Book) {
         val bookToUpdate = bookRepository.findByIsbn(isbn)?.let {
@@ -34,7 +45,7 @@ class BookService(val bookRepository: BookRepository) {
                 createdBy = book.createdBy
             )
         } ?: book
-
+        logger.info { "Book with ISBN $isbn was updated: $book" }
         bookRepository.save(bookToUpdate)
     }
 }
